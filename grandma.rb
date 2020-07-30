caefd5c46547628b64224bed63140fddfb3c7cd8
Adam Palmer <theadampalmer@gmail.com>
2020-07-30 16:41:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,17 +13,26 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "Hi Nana, how are you?"
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "Hi!"
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "WHAT DID YOU EAT TODAY?"
-    return "NO, NOT SINCE 1938!"
-  elsif phrase == "WHAT?"
-    return "NO, NOT SINCE 1938!"
-  end
-end
+# def speak_to_grandma(phrase)
+#   if phrase == "I LOVE YOU GRANDMA!"
+#     return "I LOVE YOU TOO PUMPKIN!"
+#   elsif phrase == "Hi Nana, how are you?"
+#     return "HUH?! SPEAK UP, SONNY!"
+#   elsif phrase == "Hi!"
+#     return "HUH?! SPEAK UP, SONNY!"
+#   elsif phrase == "WHAT DID YOU EAT TODAY?"
+#     return "NO, NOT SINCE 1938!"
+#   elsif phrase == "WHAT?"
+#     return "NO, NOT SINCE 1938!"
+#   end
+# end
   
+  def speak_to_grandma(phrase)
+    if phrase.upcase != phrase
+      "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == "I LOVE YOU GRANDMA!"
+      "I LOVE YOU TOO PUMPKIN!"
+    else phrase == "HI"
+      "NO, NOT SINCE 1938!"
+    end
+  end
